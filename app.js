a2c127daecb29f7f9917ae95bcd49fd7131d4b93
Wakestyle1465 <105329992+Wakestyle1465@users.noreply.github.com>
2022-05-23 20:01:14 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// document.getElementById('#container')
+// document.querySelector('#container')
+// document.querySelectorAll('.second')
+// document.querySelector('ol.third')
+// const container = document.querySelector('#container')
+// container.innerText = 'hello'
+// const div = document.querySelector('.footer')
+// div.setAttribute('class', 'main')
+// div.remove('class')
+// const ul =document.querySelector('ul')
+// const newLi = document.createElement('li')
+// newLi.innerText = 'four'
+// ul.append(newLi)
+// const ol = document.querySelectorAll('ol li')
+// for(let i = 0; i <ol.length; i++){
+//     ol.style.backgroundColor = 'green'
+// }
+// const divFooter = document.querySelector('.footer')
+// divFooter.remove
